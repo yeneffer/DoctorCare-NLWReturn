d078b65f92b157fad192c798bdc8c4eb734afe49
jeniffer <jeniffersantos43@gmail.com>
2022-06-14 03:16:18 -0300

    Adiciona responsivo e finaliza projeto

--- main.js
+++ main.js
@@ -1,10 +1,60 @@
+window.addEventListener("scroll", onScroll);
+onScroll();
+
 function onScroll() {
-  if (scrollY > 0) {
-    navigation.classList.add("scroll");
-  } else {
-    navigation.classList.remove("scroll");
+  showNavBarOnScroll();
+  showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
   }
 }
+
+function showNavBarOnScroll() {
+  if (scrollY > 0) {
+    navbar.classList.add("scroll");
+  } else {
+    navbar.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
 function openMenu() {
   document.body.classList.add("menu-expanded");
 }
@@ -13,6 +63,15 @@ function closeMenu() {
 }
 
 ScrollReveal({
-  origin: 'top'
-  
-}).reveal('#home');
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`#home, 
+#home img,
+#home .stats,
+#services,
+#services header, 
+#services .card.
+#about,
+#about header.
+#about .content.`);
